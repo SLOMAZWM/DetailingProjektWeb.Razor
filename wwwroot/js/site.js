28081d4f7511ede6386c812d837fcc50a7da9da2
Kamil Robotycki <krobotycki@wp.pl>
2024-05-15 23:48:24 +0200

    Main Page: Added products with pictures and colours

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function () {
+    var backToTopBtn = document.getElementById("backToTopBtn");
+
+    window.onscroll = function () {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 1900 || document.documentElement.scrollTop > 1900) {
+            backToTopBtn.style.display = "block";
+            backToTopBtn.classList.add("show");
+        } else {
+            backToTopBtn.classList.remove("show");
+            backToTopBtn.style.display = "none";
+        }
+    }
+
+    function backToTop() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+
+    backToTopBtn.onclick = function () {
+        backToTop();
+    };
+});
