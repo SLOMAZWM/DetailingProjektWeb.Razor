824ce7c88096cb2df2f622f0998528d02872891b
Marcin Slomka <m.slomka@student.po.edu.pl>
2024-05-25 21:53:42 +0200

    Models fixed

--- Models/User/Employee.cs
+++ Models/User/Employee.cs
@@ -5,15 +5,12 @@ namespace WebProjektRazor.Models.User
     public class Employee : User
     {
         public int EmployeeId { get; set; }
-        public string Position { get; set; }
+        public string Position { get; set; } = string.Empty;
         public ObservableCollection<Order> AssignedOrders { get; set; }
 
-        public Employee(int employeeId = 0, string position = "brak wyniku", ObservableCollection<Order> assignedOrders = null!, int userId = 0, string firstName = "brak wyniku", string lastName = "brak wyniku", string email = "brak wyniku", string password = "brak wyniku", string phoneNumber = "brak wyniku")
-            : base(userId, firstName, lastName, email, password, phoneNumber)
+        public Employee () : base ()
         {
-            EmployeeId = employeeId;
-            Position = position;
-            AssignedOrders = assignedOrders;
+            AssignedOrders = new ObservableCollection<Order>();
         }
     }
 }
