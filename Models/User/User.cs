18536dcb6f457ab3d6f2fb1fe203d198a34de309
Marcin Słomka <m.slomka@student.po.edu.pl>
2024-05-25 22:06:42 +0200

    ##SQL CREATE TABLE UPDATE

--- Models/User/User.cs
+++ Models/User/User.cs
@@ -15,28 +15,39 @@ namespace WebProjektRazor.Models.User
 
         [Required(ErrorMessage = "Imię jest wymagane.")]
         [StringLength(50, ErrorMessage = "Imie musi mieć conajmniej 3 znaki i mniej niz 50 znaków.", MinimumLength = 3)]
-        public string FirstName { get; set; } = string.Empty;
+        public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Nazwisko jest wymagane.")]
         [StringLength(50, ErrorMessage = "Nazwisko musi mieć conajmniej 3 znaki i mniej niz 50 znaków.", MinimumLength = 3)]
-        public string LastName { get; set; } = string.Empty;
+        public string LastName { get; set; }
 
         [Required(ErrorMessage = "Email jest wymagany.")]
         [EmailAddress(ErrorMessage = "Nieprawidłowy format email.")]
-        public string Email { get; set; } = string.Empty;
+        public string Email { get; set; }
 
         [Required(ErrorMessage = "Hasło jest wymagane.")]
         [StringLength(256, ErrorMessage = "Hasło musi mieć od 8 do 256 znaków", MinimumLength = 8)]
         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d\s:]).*$", ErrorMessage = "Hasło musi zawierać co najmniej jedną dużą literę, jedną małą literę, jedną cyfrę i jeden znak specjalny.")]
-        public string Password { get; set; } = string.Empty;
+        public string Password { get; set; }
 
         [Required(ErrorMessage = "Numer telefonu jest wymagany.")]
-        [RegularExpression(@"^\+\d{1,3}\s\d{1,3}\s\d{3}\s\d{3}\s\d{3}$", ErrorMessage = "Numer telefonu musi być w formacie +00 000 000 000.")]
-        public string PhoneNumber { get; set; } = string.Empty;
+        [RegularExpression(@"^\d{3}\s\d{3}\s\d{3}$", ErrorMessage = "Numer telefonu musi być w formacie 000 000 000.")]
+        public string PhoneNumber { get; set; }
 
         public UserRole Role { get; set; }
 
         public User() { }
+
+        public User(int userId = 0, string firstName = "", string lastName = "", string email = "", string password = "", string phoneNumber = "", UserRole role = UserRole.Client)
+        {
+            UserId = userId;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            PhoneNumber = phoneNumber;
+            Role = role;
+        }
 
     }
 
