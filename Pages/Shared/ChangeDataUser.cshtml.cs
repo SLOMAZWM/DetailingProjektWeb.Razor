f2b96637f227203ebb8c5d36664351354b969348
Kamil Robotycki <krobotycki@wp.pl>
2024-05-26 23:13:01 +0200

    Changing Data Almost Finished

---
+++ Pages/Shared/ChangeDataUser.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebProjektRazor.Pages.Shared
+{
+    public class ChangeDataUserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
