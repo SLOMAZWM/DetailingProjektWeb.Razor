46013ff02cbd5458e70451c870d41e3ccdac01a3
Marcin Slomka <m.slomka@student.po.edu.pl>
2024-05-27 23:27:29 +0200

    Added Products to database

--- Models/Product.cs
+++ Models/Product.cs
@@ -8,6 +8,7 @@ namespace WebProjektRazor.Models
         public string Name { get; set; } = string.Empty;
         public short Quantity { get; set; }
         public decimal Price { get; set; }
+        public string ImgPath { get; set; } = string.Empty;
 
         public virtual ICollection<OrderProducts> OrderProducts { get; set; } = new List<OrderProducts>();
     }
