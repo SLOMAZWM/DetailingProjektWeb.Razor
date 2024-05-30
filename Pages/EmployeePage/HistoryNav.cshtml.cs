ac0d32218bffaf7452c0c9a307a9f73193e0f103
kamil Robot <krobotycki@wp.pl>
2024-05-30 13:18:44 +0200

    History

---
+++ Pages/EmployeePage/HistoryNav.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebProjektRazor.Pages.EmployeePage
+{
+    public class HistoryNavModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
